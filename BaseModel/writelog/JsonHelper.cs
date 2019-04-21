69ad7a2c01db99e73ab62b789b4d0f720b153e9b
APTK008 <489567501@qq.com>
2019-04-21 22:13:09 +0800

    添加log文件

---
+++ BaseModel/writelog/JsonHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseModel.log
+{
+    public class JsonHelper
+    {
+        public static string ObjToJsonString<T>(T obj)
+        {
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            return json;
+        }
+
+        public static T JsonToObj<T>(string jsonObject)
+        {
+            T t = JsonConvert.DeserializeObject<T>(jsonObject);
+            return t;
+        }
+
+
+
+    }
+}
---
+++ BaseModel/writelog/WriteLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseModel.log
+{
+    public class WriteLog
+    {
+        private static string LogPath = AppDomain.CurrentDomain.BaseDirectory;
+        private static object objLock = new object();
+        public static void WriteLogFile(string text)
+        {
+            string saveLogPath = Path.Combine(LogPath, "log", DateTime.Now.ToString("yyyy-MM-dd"));
+            string savelogFile = Path.Combine(saveLogPath, DateTime.Now.Hour + ".log");
+            lock (objLock)
+            {
+                if (Directory.Exists(saveLogPath))
+                {
+                    File.AppendAllText(savelogFile, DateTime.Now.ToString("yyyy-MM-dd:HH-mm-ss") + Environment.NewLine + text + Environment.NewLine);
+                }
+                else
+                {
+                    Directory.CreateDirectory(saveLogPath);
+                    WriteLogFile(text);
+                }
+            }
+        }
+        public static void ConsoleWritelog(string text)
+        {
+            //text.ToCharArray().ToList().ForEach(s => { Console.WriteLine(s);Thread.Sleep(50); });
+            Console.WriteLine(text);
+            WriteLogFile(text);
+        }
+    }
+}
